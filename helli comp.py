04fdd854b01a8c63c4a0eef66f10e78af153f9e0
Portal <166227618+ParhamCoder@users.noreply.github.com>
2024-04-06 20:34:59 +0330

    Update helli comp.py
    .

--- helli comp.py
+++ helli comp.py
@@ -2,4 +2,8 @@ n = int(input())
 height = []
 if n >= 100 & n >= 2:
     for i in  range(1, (n+1)):
-        print(i)
+        a = int(input('Enter :'))
+        height.append(a)
+        height = height.sort()
+
+    print(height[1])
