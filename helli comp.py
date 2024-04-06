1fb89ba04b695df7628eef44890336c40f2980c6
Tajik89 <164622511+Tajik89@users.noreply.github.com>
2024-04-06 19:05:41 +0330

    .

--- helli comp.py
+++ helli comp.py
@@ -1,9 +1,7 @@
 n = int(input('How many people are in race?'))
 s = 0
-if n<=2 :
+if n<=2 and 100<n:
     print('You cant enter 2>n !')
-if n>=100 :
-    print('You cant enter 100<n !')
 else:
     for i in range(1, (n+1)) :
         if i>s :
