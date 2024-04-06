a56e5a9896c91cfeb51190df71b04c23b6925824
ParhamCoder <166227618+ParhamCoder@users.noreply.github.com>
2024-04-06 20:30:47 +0330

    add another code

--- helli comp.py
+++ helli comp.py
@@ -1,10 +1,13 @@
 n = int(input())
 height = []
+first = 0
 if n >= 100 & n >= 2:
     for i in range(n):
-        print(i)
+        num = int(input())
+        height.append(num)
+    for j in height:
+        if j > first:
+            first = j
 
 else:
     print('this amount of players are not valid')
-
-
