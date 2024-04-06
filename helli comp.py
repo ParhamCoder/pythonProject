28490ed246d4e096a6f56fe151680996b78b4f97
ParhamCoder <166227618+ParhamCoder@users.noreply.github.com>
2024-04-06 20:18:54 +0330

    add another cede

--- helli comp.py
+++ helli comp.py
@@ -1,13 +1,8 @@
-n = int(input('How many people are in race?'))
-s = 0
-m = 0
-if n<=2 and 100<n:
-    print('You cant enter 2>n !')
-else:
-    for i in range(1, (n+1)) :
-        if i>s :
-            s = i
-    for i in range(1, (n+1)) :
-        if s>i>m :
-            m = i
-print(m)
+n: int = int(input())
+height = []
+if n >= 100 and n >= 2:
+    for i in n:
+        num = int(input())
+        height.append(num)
+
+print(height)
