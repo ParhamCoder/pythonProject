5d84d58a788402300095014e83e6404ba54a8c18
ParhamCoder <166227618+ParhamCoder@users.noreply.github.com>
2024-04-07 05:24:43 +0330

    add the if part, to the file.

--- helli comp.py
+++ helli comp.py
@@ -1,11 +1,16 @@
 n = int(input())
 height = []
 first = 0
-for i in range(n):
-    num = int(input())
-    height.append(num)
-height2 = height
-height.sort()
-answer = height[-2]
-answer2 = height2.index(answer)
-print(answer, 'index=',answer2 + 1)
+a = 1
+if n > 100 or n < 2:
+    print('The amount of players is not valid')
+else:
+    for i in range(n):
+        num = int(input(f'Enter height {a}'))
+        height.append(num)
+        a += 1
+    height2 = height
+    height.sort()
+    answer = height[-2]
+    answer2 = height2.index(answer)
+    print(answer, 'index=', answer2 + 1)
