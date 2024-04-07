9a42a8bec9a4cc3732bb36cb207dce5582b0fae9
ParhamCoder <166227618+ParhamCoder@users.noreply.github.com>
2024-04-07 05:38:31 +0330

    if and a few other changes.

--- helli comp.py
+++ helli comp.py
@@ -1,16 +1,19 @@
 n = int(input())
 height = []
+height2 = []
 first = 0
 a = 1
-if n > 100 or n < 2:
+if n >= 100 or n <= 2:
     print('The amount of players is not valid')
 else:
     for i in range(n):
-        num = int(input(f'Enter height {a}'))
+        num = int(input(f'Enter height {a} : '))
         height.append(num)
         a += 1
-    height2 = height
+        height2.append(num)
     height.sort()
     answer = height[-2]
+    print(height2)
+    print(height)
     answer2 = height2.index(answer)
-    print(answer, 'index=', answer2 + 1)
+    print(answer, 'index=', (answer2 + 1))
